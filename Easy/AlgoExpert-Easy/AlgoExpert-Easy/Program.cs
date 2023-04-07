f3ec5e4a04033fb1d2dd6aba5239c76237682bfb
Abtahee Farhan Ador <abtahee.ador25@gmail.com>
2023-04-07 23:40:18 +0600

    Two Sum Array

---
+++ Easy/AlgoExpert-Easy/AlgoExpert-Easy/Program.cs
@@ -0,0 +1,5 @@
+﻿using AlgoExpert_Easy;
+
+int[] arr = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 };
+var output = Two_Number_Sum.TwoNumberSum(arr, 10);
+Console.WriteLine(output[0]+" "+output[1]);
---
+++ Easy/AlgoExpert-Easy/AlgoExpert-Easy/Two-Number-Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert_Easy
+{
+    public class Two_Number_Sum
+    {
+        public static int[] TwoNumberSum(int[] array, int targetSum)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int required = targetSum - array[i];
+                if (set.Contains(required))
+                {
+                    return new int[2] { required, array[i] };
+                }
+                set.Add(array[i]);
+            }
+            return new int[0];
+        }
+    }
+}
