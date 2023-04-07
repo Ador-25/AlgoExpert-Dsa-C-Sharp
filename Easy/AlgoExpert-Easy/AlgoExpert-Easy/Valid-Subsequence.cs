1d11247a7c01e00ff9f10882856ecc8217d25724
Abtahee Farhan Ador <abtahee.ador25@gmail.com>
2023-04-07 23:50:49 +0600

    valid subsequence file added

---
+++ Easy/AlgoExpert-Easy/AlgoExpert-Easy/Valid-Subsequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert_Easy
+{
+    public class Valid_Subsequence
+    {
+        public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+        {
+            int slowPointer = 0;
+            for(int i = 0; i < array.Count; i++)
+            {
+                if(array[i] == sequence[slowPointer])
+                {
+                    slowPointer++;
+                    
+                }
+            }
+            return slowPointer==sequence.Count-1;
+        }
+    }
+}
