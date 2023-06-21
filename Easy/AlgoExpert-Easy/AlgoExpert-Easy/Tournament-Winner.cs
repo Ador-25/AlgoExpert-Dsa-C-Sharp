c711b3706f8b76529e8a21b182dea8059c5ef0be
Ador-25 <abtahee.ador25@gmail.com>
2023-06-21 17:28:19 +0600

    solution 4

---
+++ Easy/AlgoExpert-Easy/AlgoExpert-Easy/Tournament-Winner.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert_Easy
+{
+    public class Tournament_Winner
+    {
+        public string TournamentWinner(List<List<string>> competitions, List<int> results)
+        {
+            // Write your code here.
+
+            Dictionary<string,int> points =  new Dictionary<string,int>();
+            int index = 0;
+            foreach(List<string> match in competitions)
+            {
+                string winner = "";
+                if (results[index] == 0)
+                {
+                    winner = match[1];
+                }
+                else
+                {
+                    winner = match[0];
+                }
+                index++;
+                if(points.ContainsKey(winner))
+                {
+                    int point = points[winner];
+                    points[winner] = point+3;
+                }
+                else
+                {
+                    points.Add(winner, 3);
+                }
+            }
+            string ans = "";
+            int max = -1;
+            foreach(string item in points.Keys)
+            {
+                if (points[item] > max)
+                {
+                    max = points[item];
+                    ans = item;
+                }
+            }
+            return ans;
+        }
+    }
+}
