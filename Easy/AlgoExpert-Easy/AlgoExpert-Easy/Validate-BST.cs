cdae7b4bfea01adcbffb3478328c00aff194805c
Ador-25 <abtahee.ador25@gmail.com>
2023-10-11 13:25:42 +0600

    valid bst

---
+++ Easy/AlgoExpert-Easy/AlgoExpert-Easy/Validate-BST.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgoExpert_Easy;
+
+public class Validate_BST
+{
+    public static bool ValidateBst(BST tree) {
+        // Write your code here.
+        return Validate(int.MinValue,int.MaxValue,tree);
+    }
+    public static bool Validate(int min,int max,BST tree)
+    {
+        if(tree==null)
+            return true;
+        if(tree.value>=min && tree.value<max)
+        {
+            return Validate(min,tree.value,tree.left) && Validate(tree.value,max,tree.right);
+        }
+        else return false;
+    }
+
+    public class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+}
